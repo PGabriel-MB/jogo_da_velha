73a0e6c1934acb1dc252e7f7cc66701944ef7620
PGabriel-MB <pablogrbarbosa@gmail.com>
2019-03-25 21:10:29 -0300

    pre-finalizaçao

--- jogo.js
+++ jogo.js
@@ -1,6 +1,22 @@
 var rodada = 1;
 var matrizJogo = Array(3);
 
+matrizJogo['a'] = Array(3);
+matrizJogo['b'] = Array(3);
+matrizJogo['c'] = Array(3);
+
+matrizJogo['a'][1] = 0;
+matrizJogo['a'][2] = 0;
+matrizJogo['a'][3] = 0;
+
+matrizJogo['b'][1] = 0;
+matrizJogo['b'][2] = 0;
+matrizJogo['b'][3] = 0;
+
+matrizJogo['c'][1] = 0;
+matrizJogo['c'][2] = 0;
+matrizJogo['c'][3] = 0;
+
 $(document).ready(function(){
 
     $('#btn_iniciarJogo').click(function(){
@@ -44,5 +60,65 @@ $(document).ready(function(){
         rodada++;
 
         $('#'+id).css('background-image', icone);
+
+        var linha_coluna = id.split('-');
+
+        matrizJogo[linha_coluna[0]][linha_coluna[1]] = ponto;
+
+        verifica_combinacao();
+    }
+
+    function verifica_combinacao(){
+        //checking horizontal
+        var pontos = 0;
+        for(let i = 1; i <= 3; i++){
+            pontos = pontos + matrizJogo['a'][i];
+        }
+        ganhador(pontos);
+
+        pontos = 0;
+        for(let i = 1; i <= 3; i++){
+            pontos = pontos + matrizJogo['b'][i];
+        }
+        ganhador(pontos);
+
+        pontos = 0;
+        for(let i = 1; i <= 3; i++){
+            pontos = pontos + matrizJogo['c'][i];
+        }
+        ganhador(pontos);
+
+        //checking vertical
+        for(let l = 1; l <= 3; l++){
+            pontos = 0;
+            pontos += matrizJogo['a'][l];
+            pontos += matrizJogo['b'][l];
+            pontos += matrizJogo['c'][l];
+
+            ganhador(pontos);
+        }
+
+        //checking diagonal
+        pontos = 0;
+        pontos = matrizJogo['a'][1] + matrizJogo['b'][2] + matrizJogo['c'][3];
+        ganhador(pontos);
+
+        pontos = 0;
+        pontos = matrizJogo['a'][3] + matrizJogo['b'][2] + matrizJogo['c'][1];
+        ganhador(pontos);
+
+    }
+
+    function ganhador(pontos){
+        if(pontos == -3){
+            var jogada_1 = $('#ipt_entradaPl1').val();
+            alert(jogada_1 + ' é o VENCEDOR!');
+            $('.jogada').off();
+        }
+        else if(pontos == -3){
+            var jogada_2 = $('#ipt_entradaPl2').val();
+            alert(jogada_2 + ' é o VENCEDOR!');
+            $('.jogada').off();
+        }
     }
 });
